3f9a9561fabdb5bf6e6552e969db3d646290c6dd
Predrag Stojadinovic <predrag@stojadinovic.net>
2019-01-19 12:43:37 +0100

    wip

--- src/prefEngine.ts
+++ src/prefEngine.ts
@@ -6,6 +6,7 @@ import PrefEngineRound from "./prefEngineRound";
 import PrefDeck, {PrefDeckDeal} from "preferans-deck-js";
 import PrefEnginePlayer from "./prefEnginePlayer";
 import PrefScore from "preferans-score-js";
+import PrefDeckCard from "preferans-deck-js/lib/prefDeckCard";
 
 export type PrefEngineOptions = {
 	unlimitedRefe: boolean,
@@ -22,24 +23,26 @@ const random = (p1: PrefEnginePlayer, p2: PrefEnginePlayer, p3: PrefEnginePlayer
 	return r === 1 ? p1 : r === 2 ? p2 : p3;
 };
 
-export enum PrefEngineDealRole {DEALER, LEFT, RIGHT}
+export enum PrefEngineDealRole {DEALER, SECOND_BIDDER, FIRST_BIDDER}
 
 export default class PrefEngine {
+	private readonly _bula: number;
+	private readonly _refas: number;
+	private readonly _options: PrefEngineOptions;
+
 	private _p1: PrefEnginePlayer;
 	private _p2: PrefEnginePlayer;
 	private _p3: PrefEnginePlayer;
-	private readonly _bula: number;
-	private readonly _refas: number;
+
 	private _deck: PrefDeck;
 	private _score: PrefScore;
 	private _round: PrefEngineRound;
 	private _rounds: PrefEngineRound[];
-	private readonly _options: PrefEngineOptions;
 
 	constructor(p1: string, p2: string, p3: string, bula: number, refas: number, options: PrefEngineOptions) {
-		this._p1 = new PrefEnginePlayer(p1, PrefEngineDealRole.RIGHT);
-		this._p2 = new PrefEnginePlayer(p1, PrefEngineDealRole.LEFT);
-		this._p3 = new PrefEnginePlayer(p1, PrefEngineDealRole.DEALER);
+		this._p1 = new PrefEnginePlayer(p1, PrefEngineDealRole.FIRST_BIDDER);
+		this._p2 = new PrefEnginePlayer(p1, PrefEngineDealRole.DEALER);
+		this._p3 = new PrefEnginePlayer(p1, PrefEngineDealRole.SECOND_BIDDER);
 		this._bula = bula;
 		this._refas = refas;
 		this._options = options;
@@ -51,17 +54,18 @@ export default class PrefEngine {
 		this._round = new PrefEngineRound(this._deck, this._p1, this._p2, this._p3);
 	}
 
+	restoreDeck(cards: PrefDeckCard[]): PrefEngine {
+		this._deck.restore(cards);
+		return this;
+	}
+
 	deal(): PrefEngine {
-		let previousDealer = this._third;
-		this._third = this._first;
-		this._first = this._second;
-		this._second = previousDealer;
+		let tmp = this._p3;
+		this._p3 = this._p1;
+		this._p1 = this._p2;
+		this._p2 = tmp;
 
-		this._third = random(this._p1, this._p2, this._p3);
-		this._first = this.next(this._third);
-		this._second = this.next(this._first);
-
-		this._round = new PrefEngineRound(this._deck.deal, this._first, this._second, this._third);
+		this._round = new PrefEngineRound(this._deck, this._p1, this._p2, this._p3);
 		return this;
 	}
 
@@ -89,10 +93,11 @@ export default class PrefEngine {
 		// TODO: verify stage and user
 	}
 
-	private next(user: string): string {
-		if (user === this._p1) return this._p2;
-		if (user === this._p2) return this._p3;
-		return this._p1;
-	};
+	private next(p: PrefEnginePlayer): PrefEnginePlayer {
+		if (p.username === this._p1.username) return this._p2;
+		if (p.username === this._p2.username) return this._p3;
+		if (p.username === this._p3.username) return this._p1;
+		throw new Error("PrefEngine::next:Unrecognized user: " + p);
+	}
 
 }
--- src/prefEnginePlayer.ts
+++ src/prefEnginePlayer.ts
@@ -3,20 +3,31 @@
 
 import {PrefEngineDealRole} from "./prefEngine";
 
-export enum PrefEnginePlayerRole {NONE = 0, MAIN, LEFT, RIGHT}
+export enum PrefEnginePlayerRole {NONE = 0, MAIN, RIGHT_FOLLOWER, LEFT_FOLLOWER}
 
 export default class PrefEnginePlayer {
-	private _username: string;
+	private readonly _starter: string;
 	private readonly _dealRole: PrefEngineDealRole;
+	private readonly _replacements: string[];
+
+	private _username: string;
 	private _playRole: PrefEnginePlayerRole;
 
-	private _replacements: string[];
-
 	constructor(username: string, dealRole: PrefEngineDealRole) {
+		this._starter = username;
 		this._username = username;
 		this._dealRole = dealRole;
 		this._playRole = PrefEnginePlayerRole.NONE;
 		this._replacements = [];
+	}
+
+	set replacement(username: string) {
+		this._username = username;
+		if (this._starter !== username) this._replacements.push(username);
+	}
+
+	get starter(): string {
+		return this._starter;
 	}
 
 	get username(): string {
